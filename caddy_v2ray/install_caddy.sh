cd99a22244e240a9c998a1f623b4b9be4175ac13
Ganben <caozhao@gmail.com>
2019-02-16 23:59:51 +0800

    add certbot

--- caddy_v2ray/install_caddy.sh
+++ caddy_v2ray/install_caddy.sh
@@ -37,3 +37,12 @@ WantedBy=multi-user.target
 
 # v2ray configs
 # TODO
+# install certbot for ssl certificate
+
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo add-apt-repository universe
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install certbot python-certbot-nginx 
+sudo certbot --nginx
