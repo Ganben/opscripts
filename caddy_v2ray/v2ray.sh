f35494d72de820b6eabae41824d68e42dbad3840
Ganben <caozhao@gmail.com>
2019-05-17 14:49:31 +0800

    vy test pass

---
+++ caddy_v2ray/v2ray.sh
@@ -0,0 +1,18 @@
+# !/bin/bash
+# install, see https://github.com/v2ray/manual/blob/master/zh_cn/chapter_00/install.md
+sudo su
+
+bash <(curl -L -s https://install.direct/go.sh)
+
+# /etc/v2ray/config.json
+# mode: vmess + wss
+
+# then service v2ray start
+
+
+# client config json
+echo "
+" > "config.json"
+
+
+# server inbound conf
